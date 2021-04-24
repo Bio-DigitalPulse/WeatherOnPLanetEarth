fbe321401f4aab756776ac9dee604f78d38d3aaa
aaron.k.keller@gmail.com <aaron.k.keller@gmail.com>
2021-04-24 00:39:45 -0400

    Added comments and links

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ This project is a Weather Dashboard App entitled Weather On Planet Earth. Utiliz
 Search history for this application is stored in local storage, and is fully selectable within an appended list, under the search function. This list will show all the prior searches that you have made, making for easy identification of favorite locations. Included within this data is location name, the current date, the temperature in Fahrenheit and Celsius, the humidity rating, the wind speed, the UV rating along with a colored icon that will change depending on the severity of the UV range, and a forecast for the next five days.
 
 
+The link to view the dashboard on Github is here:
+https://github.com/ascendedkrishna/WeatherOnPLanetEarth.git
+
+The link to view the weather dashboards live webpage is here:
+https://ascendedkrishna.github.io/WeatherOnPLanetEarth/
--- script.js
+++ script.js
@@ -1,10 +1,10 @@
   
 
-var apiKey = "d735412b94d837506da35310984787e4";
+var apiKey = "d735412b94d837506da35310984787e4";        //API key to call data
 
 $(document).ready(function(){
 
-    $("#searchButton").on("click",searchLocation);
+    $("#searchButton").on("click",searchLocation);      //Search function examining localStorage for recent history
 
     var history = JSON.parse(localStorage.getItem("history"));  
 
@@ -19,31 +19,30 @@ $(document).ready(function(){
         }
     } else {
         $("#city-card").hide();
-        $("#forecastDisplay").hide();
+        $("#forecastDisplay").hide();       //If there exists history, append it, otherwise hide card and forecast
     }
 });
 
-$("#historylist").on("click", "li", function(event){
+$("#historylist").on("click", "li", function(event){        //Click event for appended cities
 
     var previousCityName = $(this).text();
-    console.log("Previous city : "+ previousCityName);
     currentLocationWeather(previousCityName);
 });
  
-function searchLocation(event){
+function searchLocation(event){     //Search function to search location
     event.preventDefault();
 
-    var searchInput = $("#citySearch").val();
+    var searchInput = $("#citySearch").val();       //Checks to see if there is any input, if there is then return
     if(searchInput === "")
     {
         return;
     } 
-    currentLocationWeather(searchInput);
+    currentLocationWeather(searchInput);        //Return these functons
     createSearchHistory(searchInput);
     $("#citySearch").val("");
 }
 
-function createSearchHistory(city){
+function createSearchHistory(city){     //Once a search is made, the request gets recorded to the list in localStorage
 
     var history = JSON.parse(localStorage.getItem("history"));  
     var listitem;
@@ -55,22 +54,21 @@ function createSearchHistory(city){
                 return;
             }         
         } 
-        history.unshift(city); 
+        history.unshift(city);      //Else statement adding or removing search history to localStorage
         listitem = $("<li>").addClass("list-group-item previousCity").text(city);
         $("#historylist").prepend(listitem);    
     }
     else{
-            history = [city]; 
-            listitem = $("<li>").addClass("list-group-item previousCity").text(city);
-            $("#historylist").append(listitem);
-        }
+        history = [city]; 
+        listitem = $("<li>").addClass("list-group-item previousCity").text(city);
+        $("#historylist").append(listitem);
+    }
     localStorage.setItem("history", JSON.stringify(history));   
 }
 
 
-function showForecast(forecastQuery){
-
-
+function showForecast(forecastQuery){       //Function that creates cards displaying the forcast, adding various elements including icon
+    
     var temp, humidity,icon;
 
     $.ajax({
@@ -78,7 +76,7 @@ function showForecast(forecastQuery){
         url: forecastQuery,
         method: "GET"
 
-    }).then(function(forecastData){
+    }).then(function(forecastData){         
 
         $("#forecast").empty();
         var list = forecastData.list;
@@ -86,7 +84,7 @@ function showForecast(forecastQuery){
         for(var i = 0 ; i < list.length ;i++){
             var date = list[i].dt_txt.split(" ")[0];
             var dateArr = date.split("-");
-            var dateForecast = dateArr[1]+"/"+dateArr[2]+"/"+dateArr[0];
+            var dateForecast = dateArr[1] + "/" + dateArr[2] + "/" + dateArr[0];
             var time = list[i].dt_txt.split(" ")[1];
             
             if(time === "12:00:00"){
@@ -99,8 +97,8 @@ function showForecast(forecastQuery){
                 var cardBody = $("<div>").addClass("card-body");
                 var fDate = $("<h5>").addClass("card-text").text(dateForecast);
                 var imgIcon = $("<img>").attr("src","https://openweathermap.org/img/wn/" + icon + ".png"); 
-                var tempP  = $("<p>").addClass("card-text").text("Temp: "+temp+"°F");
-                var humidityP = $("<p>").addClass("card-text").text("Humidity : "+humidity+" % ");
+                var tempP  = $("<p>").addClass("card-text").text("Temp: " + temp + "°F");
+                var humidityP = $("<p>").addClass("card-text").text("Humidity : " + humidity + " % ");
 
                 cardBody.append(fDate, imgIcon, tempP, humidityP);
                 card.append(cardBody);
@@ -113,35 +111,34 @@ function showForecast(forecastQuery){
 }
 
 
-function currentLocationWeather(city){
+function currentLocationWeather(city){      //Function pulling current location weather information
 
-    var searchQueryURL = "https://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+ apiKey; 
+    var searchQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + apiKey; 
 
     $.ajax({
         url: searchQueryURL,
         method: "GET"
 
-    }).then(function(response){
+    }).then(function(response){         //Creates card displaying the data for the user
  
+        var latitude = response.coord.lat;
+        var longitude = response.coord.lon;
         var tempFahr = (response.main.temp - 273.15) * 1.80 + 32;
         var tempCels = (response.main.temp - 273.15);
         var currentDate = new Date().toLocaleDateString();
-        var latitude = response.coord.lat;
-        var longitude = response.coord.lon;
-        
 
-        var uvQuery = "https://api.openweathermap.org/data/2.5/uvi?lat="+latitude+"&lon="+longitude+"&appid="+ apiKey;
+        var uvQuery = "https://api.openweathermap.org/data/2.5/uvi?lat=" + latitude + "&lon=" + longitude + "&appid=" + apiKey;
         var cityID = response.id;
-        var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?id="+cityID+"&units=imperial&appid="+apiKey;
+        var forecastQuery = "https://api.openweathermap.org/data/2.5/forecast?id=" + cityID + "&units=imperial&appid=" + apiKey;
           
         $("#city-card").show();
-        $("#temperature").text("Temperature : "+tempFahr.toFixed(2)+" °F/ "+tempCels.toFixed(2)+"°C");
-        $("#humidity").text("Humidity : "+response.main.humidity+" %");
-        $("#windspeed").text("Wind Speed : "+response.wind.speed+" MPH");
+        $("#temperature").text("Temperature : " + tempFahr.toFixed(2) + "°F/" + tempCels.toFixed(2) + "°C");
+        $("#humidity").text("Humidity : " + response.main.humidity + "%");
+        $("#windspeed").text("Wind Speed : " + response.wind.speed + "MPH");
 
         var imageIcon = $("<img>").attr("src", "https://openweathermap.org/img/wn/" + response.weather[0].icon.toString() + ".png");
 
-        $("#city-name").text(response.name + " ("+currentDate+") ").append(imageIcon);
+        $("#city-name").text(response.name + " (" + currentDate + ") ").append(imageIcon);
 
         collectUV(uvQuery); 
         showForecast(forecastQuery);
@@ -151,7 +148,7 @@ function currentLocationWeather(city){
 }
 
 
-function collectUV(uvQuery){
+function collectUV(uvQuery){        //Function pulling UV data, and depending on that data, displaying a color code
     
     $.ajax({
         url: uvQuery,
@@ -162,7 +159,7 @@ function collectUV(uvQuery){
         var uvRange = uvStatus.value;
         var uvItem = $("<button>").attr("type","button").text(uvRange);
  
-        if(uvRange >= 0 && uvRange <= 4){
+        if(uvRange >= 0 && uvRange <= 4){           //Color range
 
             $("#uvrange").text("UV : Low, ").append(uvItem);
             uvItem.addClass("btn bg-success");
